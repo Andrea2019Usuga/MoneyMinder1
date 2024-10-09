a52aa1c3eb83b076cf3ea6d1f05e8b2256dd432f
Andrea2019Usuga <andreausuga2099@gmail.com>
2024-10-09 08:11:08 -0500

    Eliminar archivos no necesarios de la rama 2099

--- app/Views/js/24cerrar sesión.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const backToLoginButton = document.querySelector(".button-link button");
-
-    backToLoginButton.addEventListener("click", function() {
-        window.location.href = "1pagina principal.html";
-    });
-});
